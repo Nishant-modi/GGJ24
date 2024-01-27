b046c00ce57cffec5c91731d7d8b9bf66f14ff1f
Amasha Das <125284502+amasha-d@users.noreply.github.com>
2024-01-28 00:43:16 +0530

    Level1 Changes for pots and pngs

--- Assets/Scripts/FallingObjects.cs
+++ Assets/Scripts/FallingObjects.cs
@@ -18,9 +18,9 @@ public class FallingObjects : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            Debug.Log("Pot gira");
+            rb.isKinematic = true;
         }
     }
 }
