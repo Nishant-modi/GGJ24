f4b384af32be76a120a43158fa4577cef52b1ff2
Nishant-modi <mnishant0123@gmail.com>
2024-01-27 23:50:39 +0530

    dialogue man

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class DialogManager : MonoBehaviour
 {
-    public Camera mainCamera;
-    public float zoomInSize = 5f;  // Adjust the zoom-in size
-    public float zoomOutSize = 10f;  // Adjust the zoom-out size
+    //public Camera mainCamera;
+    [Header("Camera Controls")]
+    public FollowCamera fc;
+    public GameObject target1;
+    public GameObject target2;
+    public Vector3 offset1;
+    public Vector3 offset2;
+    public float zoomInSize;  // Adjust the zoom-in size
+    public float zoomOutSize;  // Adjust the zoom-out size
+
+    [Header("Dialogue Controls")]
+    public Queue<string> sentences;
+
+    private void Start()
+    {
+        fc = FindObjectOfType<FollowCamera>();
+        StartCoroutine(DialogCoroutine());
+
+        sentences = new Queue<string>();
+    }
 
     public void StartDialog()
     {
@@ -15,12 +32,21 @@ public class DialogManager : MonoBehaviour
 
     private IEnumerator DialogCoroutine()
     {
+        fc.orthographicSize = zoomInSize;
 
-        Camera.main.orthographicSize = zoomInSize;
-
+        fc.target1 = target2;
+        fc.offset = offset2;
+        
         // Start dialog here (you can call your dialog system here)
 
-        Camera.main.orthographicSize = zoomOutSize;
+        yield return new WaitForSeconds(3f);
+
+        fc.target1 = target1;
+        fc.offset = offset1;
+
+        //fc.orthographicSize = zoomOutSize;
+
+        
 
         yield return null;
     }
--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -11,18 +11,28 @@ public class FollowCamera : MonoBehaviour
     public float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
     public Vector3 minValues, maxValue;
+    public float orthographicSize = 5.2f;
 
+    private Camera mainCamera;
 
+    private void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
 
     void FixedUpdate()
     {
-        target = (target1.transform.position);
-        Vector3 targetPosition = new Vector3(target.x + offset.x, target.y + offset.y, -10f);
-        Vector3 boundPosition = new Vector3(
-           Mathf.Clamp(targetPosition.x, minValues.x, maxValue.x),
-           Mathf.Clamp(targetPosition.y, minValues.y, maxValue.y),
-           Mathf.Clamp(targetPosition.z, minValues.z, maxValue.z));
-       
-        transform.position = Vector3.SmoothDamp(transform.position, boundPosition, ref velocity, smoothTime);
+            mainCamera.orthographicSize = orthographicSize;
+            target = (target1.transform.position);
+            Vector3 targetPosition = new Vector3(target.x + offset.x, target.y + offset.y, -10f);
+            Vector3 boundPosition = new Vector3(
+               Mathf.Clamp(targetPosition.x, minValues.x, maxValue.x),
+               Mathf.Clamp(targetPosition.y, minValues.y, maxValue.y),
+               Mathf.Clamp(targetPosition.z, minValues.z, maxValue.z));
+
+            transform.position = Vector3.SmoothDamp(transform.position, boundPosition, ref velocity, smoothTime);
+        
     }
+
+
 }
